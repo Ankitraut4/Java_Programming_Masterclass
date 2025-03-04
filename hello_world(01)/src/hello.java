d114fec8883fbf8fc58c4fcb033e54663a79543a
Ankit Raut <ankitraut9421@gmail.com>
2025-03-04 10:00:00 -0500

    Add hello_world(01)/ Java Masterclass practice

---
+++ hello_world(01)/src/hello.java
@@ -0,0 +1,17 @@
+public class hello {
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+        int myfirstnumber =(5*6)+(7*7);
+        int mysecondnumber=12;
+        int mythirdnumber=6;
+        int mytotalnumber=myfirstnumber+mysecondnumber+mythirdnumber;
+        System.out.println("my total is = " +
+                "" +mytotalnumber);
+
+        int mylastone =1000-mytotalnumber;
+        System.out.println(mylastone);
+
+
+    }
+}
