bec42399ee1482464c3c601cd6b11103e618482b
Ankit Raut <ankitraut9421@gmail.com>
2025-03-08 10:00:00 -0500

    Add booleanandchar/ Java Masterclass practice

---
+++ booleanandchar/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        char mycharacteris ='a';
+        System.out.println(mycharacteris);
+
+        char myunioncharnumberis = '\u0061';
+        System.out.println(myunioncharnumberis);
+
+
+        boolean mybooleanvalueis =  true;
+        boolean mybooleanvalueforfalse =  false;
+
+
+    }
+}
