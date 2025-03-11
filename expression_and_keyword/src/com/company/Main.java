4f5826a254226f3c10dcad7b1ab51074771fa088
Ankit Raut <ankitraut9421@gmail.com>
2025-03-11 10:00:00 -0400

    Add expression_and_keyword/ Java Masterclass practice

---
+++ expression_and_keyword/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int score = 100;//here score = 100 is an expression except ";" (colon)
+        if(score>99){ //here inside of bracket score>99 is an expression
+            System.out.println("you got some idea of exp");//here inside of bracket "you got some idea of exp" is an expression
+            score=0;// score=0 is an expression except ";"
+
+              
+
+        }
+    }
+}
