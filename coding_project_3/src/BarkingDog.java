68b90bc3237083d7d3ba3c2c86a320d56dac7d78
Ankit Raut <ankitraut9421@gmail.com>
2025-03-17 10:00:00 -0400

    Add coding_project_3/ Java Masterclass practice

---
+++ coding_project_3/src/BarkingDog.java
@@ -0,0 +1,16 @@
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking,int hourOfDay){
+        if(hourOfDay>0 && hourOfDay<23) {
+            if ((barking) && (hourOfDay<8 || hourOfDay>22)) {
+                return true;
+            } else {
+                return false;
+            }
+        }else if(hourOfDay<0 || hourOfDay>23) {
+            return false;
+        }
+       return true;
+
+    }
+}
---
+++ coding_project_3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        boolean res = BarkingDog.shouldWakeUp(true, 24);
+        System.out.println(res);
+
+    }
+}
