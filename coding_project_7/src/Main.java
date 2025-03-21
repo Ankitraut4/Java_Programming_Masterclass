65f94e0b5bd0483205b3829e8adaf3a2686b6425
Ankit Raut <ankitraut9421@gmail.com>
2025-03-21 10:00:00 -0400

    Add coding_project_7/ Java Masterclass practice

---
+++ coding_project_7/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean res =TeenNumberChecker.hasTeen(13,55,66);
+        System.out.println(res);
+
+        res =TeenNumberChecker.hasTeen(10,55,66);
+        System.out.println(res);
+
+        res =TeenNumberChecker.hasTeen(10,16,66);
+        System.out.println(res);
+
+    }
+}
