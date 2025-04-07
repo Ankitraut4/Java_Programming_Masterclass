f5899bc8ac7358ebe9c946dae6120686eb40871a
Ankit Raut <ankitraut9421@gmail.com>
2025-04-07 10:00:00 -0400

    Add coding_project_15/ Java Masterclass practice

---
+++ coding_project_15/src/NumberPalindrome.java
@@ -0,0 +1,20 @@
+public class NumberPalindrome {
+
+    public static boolean isPalindrome(int number) {
+        int reverse = 0;
+        int num=number;
+
+        while (num != 0) {
+            int lastDigit = num % 10;
+            reverse = (reverse * 10) + lastDigit;
+            num /= 10;
+
+
+        }
+
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+}
