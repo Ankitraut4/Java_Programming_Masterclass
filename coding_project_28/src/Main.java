1592bf7314d021e357405d473dc6680600893930
Ankit Raut <ankitraut9421@gmail.com>
2025-04-24 10:00:00 -0400

    Add coding_project_28/ Java Masterclass practice

---
+++ coding_project_28/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PaintJob.getBucketCount(0.0,2.1,1.5,7));
+        System.out.println(PaintJob.getBucketCount(3.4,2.1,1.5));
+        System.out.println(PaintJob.getBucketCount(6.26,2.2));
+    }
+}
