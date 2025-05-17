a1f772bd3af723b2d0eae90d902a1d2fb8cdfb01
Ankit Raut <ankitraut9421@gmail.com>
2025-05-17 10:00:00 -0400

    Add coding_project_39/ Java Masterclass practice

---
+++ coding_project_39/src/com/company/Holden.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Holden extends Car{
+    public Holden(int cylinder, String name) {
+        super(cylinder, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Holden -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Holden -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Holden -> brake()";
+    }
+}
