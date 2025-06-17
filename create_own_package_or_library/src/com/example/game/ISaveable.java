fd1cad5127131b268ce9b14480c142bab47915a4
Ankit Raut <ankitraut9421@gmail.com>
2025-06-17 10:00:00 -0400

    Add create_own_package_or_library/ Java Masterclass practice

---
+++ create_own_package_or_library/src/com/example/game/ISaveable.java
@@ -0,0 +1,9 @@
+package com.example.game;
+
+import java.util.List;
+
+public interface ISaveable {
+List<String> write();//here we use List instead of and medium(i.e ArrayList) which we will define when final execution is required
+void read(List<String> savedValues);
+
+}
