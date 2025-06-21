8e8ae15f33e581ad4f9dffc4f214745df2659e04
Ankit Raut <ankitraut9421@gmail.com>
2025-06-21 10:00:00 -0400

    Add Scope_Challange/ Java Masterclass practice

---
+++ Scope_Challange/src/com/company/X.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+
+public class X {
+private int x;
+
+    public X(int x) {//or public X(Scanner x)
+        this.x =x;//this.x=x.Scanner;
+    }
+
+    public void x(){
+        for (int i=1;i<=12;i++){
+            System.out.println(i+" times "+this.x+" equal to "+(i*this.x));
+        }
+    }
+
+}
