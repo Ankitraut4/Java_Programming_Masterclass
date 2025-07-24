a5d5f300a5968d2b851da1b65e9963cc2da6787c
Ankit Raut <ankitraut9421@gmail.com>
2025-07-24 10:00:00 -0400

    Add SceneBuilder/ Java Masterclass practice

---
+++ SceneBuilder/src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label label;
+
+    public void handlePress(){
+        label.setText("you press ok button   .......");
+    }
+}
